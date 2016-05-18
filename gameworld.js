f38b73818bb6f8009467a960ee6f35e3a04bf4ca
Antonio <avalvillar@gmail.com>
2016-05-18 09:59:30 -0700

    Platform changed

--- gameworld.js
+++ gameworld.js
@@ -25,41 +25,6 @@ Platform.prototype.draw = function (ctx) {
 }
 
 var setupWorld = function (game) {
-  /*  for (var i = 0; i < 400; i+=70) {
-        var ground = new Platform(game, i, 530);
-        game.addPlatform(ground);
-    }
-
-    for (var i = 600; i > 0; i -= 70) {
-        var wall = new Platform(game, 1000, i);
-        game.addPlatform(wall);
-    }
-
-    var plat = new Platform(game, 700, 350);
-    var plat2 = new Platform(game, 770, 350);
-    game.addPlatform(plat);
-    game.addPlatform(plat2);
-
-    var plat = new Platform(game, 550, 200);
-    var plat2 = new Platform(game, 480, 200);
-    game.addPlatform(plat);
-    game.addPlatform(plat2);
-
-    var plat = new Platform(game, 400, 350);
-    var plat2 = new Platform(game, 330, 350);
-    game.addPlatform(plat);
-    game.addPlatform(plat2);
-
-    for (var i = 800; i < 2000; i += 70) {
-        var ground = new Platform(game, i, 530);
-        game.addPlatform(ground);
-    }
-
-    for (var i = 0; i < 2000; i += 70) {
-        var ceiling = new Platform(game, i, -60);
-        game.addPlatform(ceiling);
-    } */
-
 
     ///world creation
     for (var i = 0; i < 830; i += 70) {
