67510ba7da8805da7e743d0c983f2f48c5b55f28
ghoule636 <ghoule636@gmail.com>
2016-05-25 01:09:03 -0700

    Added basic sprites and animations for the crashed ship and ship part.

---
+++ objects.js
@@ -0,0 +1,41 @@
+function ship(game, x, y) {
+    this.smoking = new Animation(ASSET_MANAGER.getAsset("./img/CrashedShip.png"), 0, 0, 194, 200, .5, 2, true, false);
+    this.x = x;
+    this.y = y;
+    this.collisionX = this.x + 50;
+    this.collisionY = this.y + 85;
+    Entity.call(this, game, this.x, this.y, this.collisionX, this.collisionY);
+}
+
+ship.prototype = new Entity();
+ship.prototype.constructor = ship;
+
+ship.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+ship.prototype.draw = function (ctx) {
+    this.smoking.drawFrame(this.game.clockTick, ctx, this.x, this.y, 3);
+    Entity.prototype.draw.call(this);
+}
+
+function shipPart(game, x, y) {
+    this.smoking = new Animation(ASSET_MANAGER.getAsset("./img/ShipPart.png"), 0, 0, 33.5, 57, .2, 4, true, false);
+    this.x = x;
+    this.y = y;
+    this.collisionX = this.x + 50;
+    this.collisionY = this.y + 85;
+    Entity.call(this, game, this.x, this.y, this.collisionX, this.collisionY);
+}
+
+shipPart.prototype = new Entity();
+shipPart.prototype.constructor = shipPart;
+
+shipPart.prototype.update = function () {
+    Entity.prototype.update.call(this);
+}
+
+shipPart.prototype.draw = function (ctx) {
+    this.smoking.drawFrame(this.game.clockTick, ctx, this.x, this.y, 3);
+    Entity.prototype.draw.call(this);
+}
